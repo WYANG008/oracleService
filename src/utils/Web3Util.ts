78261771633244f61d6a0a7ddcc906aa98b8d085
Weitao Yang <weitao.yang@finbook.co>
2018-12-08 23:31:37 +0800

    add web3Util

--- src/utils/Web3Util.ts
+++ src/utils/Web3Util.ts
@@ -1,12 +1,13 @@
 // fix for @ledgerhq/hw-transport-u2f 4.28.0
 import '@babel/polyfill';
 import Web3 from 'web3';
+import { Signature } from 'web3/types';
 // import * as CST from '../common/constants';
 // import util from './util';
 
-const Web3Eth = require('web3-eth');
+// const Web3Eth = require('web3-eth');
 // const Web3Accounts = require('web3-eth-accounts');
-const Web3Personal = require('web3-eth-personal');
+// const Web3Personal = require('web3-eth-personal');
 // const Web3Utils = require('web3-utils');
 
 export enum Wallet {
@@ -39,13 +40,13 @@ export default class Web3Util {
 			? new Web3.providers.HttpProvider(provider)
 			: new Web3.providers.WebsocketProvider(provider)
 			this.web3 = new Web3(
-				provider
+				providerEngine
 			);
 			
 			this.wallet = Wallet.Local;
 		}
-		this.web3Eth = new Web3Eth(providerEngine);
-		this.Web3Personal = new Web3Personal(providerEngine);
+		// this.web3Eth = new Web3Eth(providerEngine);
+		// this.Web3Personal = new Web3Personal(providerEngine);
 	}
 
 
@@ -53,9 +54,14 @@ export default class Web3Util {
 		return this.web3Eth.getGasPrice();
 	}
 
-	public sign(data: string, key: string) {
+	public async sign(data: string, key: string):Promise<string|Signature> {
 		return this.web3.eth.accounts.sign(data, key);
 	}
+
+	public async recover(message: string, v:string, r: string, s: string) {
+		return this.web3.eth.accounts.recover(message, v, r, s);
+	}
+
 
 	// public web3PersonalSign(account: string, message: string): Promise<string> {
 	// 	if (this.wallet !== Wallet.MetaMask) return Promise.reject();
